4f85d2425b9bb9d913dbc528930ecb268aff5efb
kighas <elias.ronnmark@elev.ga.ntig.se>
2024-03-17 20:25:16 +0100

    bas på nya sidor, fortsatt mobil anpassning och aside länkar

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
-// Denna fil skall innehålla JavaScript (JS)
+const hamburger = document.querySelector("#hamburger")
+const nav = document.querySelector("nav")
+
+function toggleMenu() {
+    nav.classList.toggle("active")
+}
